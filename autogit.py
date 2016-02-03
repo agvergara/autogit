c1b20820ad71cb9bb0d8dfe732a31addd6ba1277
Antonio Gomez Vergara <antonio.g.vergara@gmail.com>
2016-02-03 13:07:54 +0100

    Update autogit.py

--- autogit.py
+++ autogit.py
@@ -13,14 +13,25 @@ try:
     localdate = time.strftime("%x")
     multibool = 0
 
-    def autogit (filepath, message, filename, multibool):
-    	#subprocess.call(["git", "init"])
-    	#filepathsplit = filepath.split('/')
-    	#filename = filepathsplit[-1]
-    	#path = "/".join(filepathsplit[:-1])
+    def autoremove (filepath, filename, multibool):
         os.chdir(filepath)
-        subprocess.call (["git", "status"])
-
+        print "WARNING: Deleting the file(s) on GitHub also deletes them from your directory"
+        confirm = raw_input("Are you sure? (y/n): ")
+        if confirm == 'y':
+            if multibool == 0:
+                subprocess.call(["git", "rm", filename], stdout=trashcan)
+            else:
+                for name in filename:
+                    subprocess.call(["git", "rm", name], stdout=trashcan)
+            subprocess.call(["git", "commit", "-m", "DELETED"], stdout=trashcan)
+            subprocess.call(["git", "push", "-f"])
+        elif confirm == 'n':
+            print "ABORTED"
+        else:
+            print "Something happened (./autogit -h for help)"
+        
+    def autopush (filepath, message, filename, multibool):
+        os.chdir(filepath)
         if multibool == 0:
             subprocess.call(["git", "add", filename], stdout=trashcan)
         else:
@@ -38,23 +49,34 @@ try:
         print "-h displays this message"
         print "-m introduce a message to commit (by default the date)"
         print "-s add multiple files"
+        print "-r delete the file"
+        print "-R delete multiple files"
         print "================"
         print "WARNING: you must have git configured (git config)"
 
     if arglength == 2:
-    	filepath = sys.argv[1]
+        filepath = sys.argv[1]
         filename = sys.argv[2]
-        autogit(filepath, localdate, filename, multibool)
-    elif arglength == 3 and option == "-m":
+        autopush(filepath, localdate, filename, multibool)
+    elif arglength == 4 and option == "-m":
         filepath = sys.argv[2]
         message = sys.argv[3]
         filename = sys.argv[4]
-        autogit(filepath, message, filename, multibool)
+        autopush(filepath, message, filename, multibool)
     elif arglength > 3 and option == "-s":
         filelist = sys.argv[3:]
         filepath = sys.argv[2]
         multibool = 1;
-        autogit(filepath, localdate, filelist, multibool)
+        autopush(filepath, localdate, filelist, multibool)
+    elif arglength == 3 and option == '-r':
+        filepath = sys.argv[2]
+        filename = sys.argv[3]
+        autoremove(filepath, filename, multibool)
+    elif arglength > 3 and option == '-R':
+        filelist = sys.argv[3:]
+        filepath = sys.argv[2]
+        multibool = 1;
+        autoremove(filepath, filelist, multibool)
     elif option == "-h":
         print help()
     else:
