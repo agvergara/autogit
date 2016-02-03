7e95af8275d6d2a2f934773c6e31e04b4b78ac18
Antonio Gomez Vergara <antonio.g.vergara@gmail.com>
2016-02-03 11:24:47 +0100

    Now with multiple uploads!

--- autogit.py
+++ autogit.py
@@ -10,36 +10,51 @@ try:
     actualpath = os.getcwd()
     arglength = len(sys.argv[1:])
     option = sys.argv[1]
+    localdate = time.strftime("%x")
+    multibool = 0
 
-    def autogit (filepath, message):
+    def autogit (filepath, message, filename, multibool):
     	#subprocess.call(["git", "init"])
-    	filepathsplit = filepath.split('/')
-    	filename = filepathsplit[-1]
-    	path = "/".join(filepathsplit[:-1])
-        os.chdir(path)
-        subprocess.call(["git", "add", filename], stdout=trashcan)
+    	#filepathsplit = filepath.split('/')
+    	#filename = filepathsplit[-1]
+    	#path = "/".join(filepathsplit[:-1])
+        os.chdir(filepath)
+        subprocess.call (["git", "status"])
+
+        if multibool == 0:
+            subprocess.call(["git", "add", filename], stdout=trashcan)
+        else:
+            for name in filename:
+                subprocess.call(["git", "add", name], stdout=trashcan)
         subprocess.call(["git", "commit", "-m", message], stdout=trashcan)
-        subprocess.call(["git", "push"])
+        subprocess.call(["git", "push", "-f"])
 
     def help():
-        print "Simple usage: ./autogit.py [OPTION] PATH MESSAGE URL"
-        print "PATH = the path to the file, e.g: /home/XXX/clonedrepository/example.py"
+        print "Simple usage: ./autogit.py [OPTION] PATH MESSAGE file1 file2 .. fileN"
+        print "PATH = the path to the file (must be absolute), e.g: /home/XXX/clonedrepository/"
         print "URL = the url of the GitHub repository"
+        print "MESSAGE = the message to commit, only with '-m' option"
         print "OPTIONS \n==============="
         print "-h displays this message"
         print "-m introduce a message to commit (by default the date)"
+        print "-s add multiple files"
         print "================"
         print "WARNING: you must have git configured (git config)"
 
-    if arglength == 1 and option != "-h" and option != "-m":
+    if arglength == 2:
     	filepath = sys.argv[1]
-        localtime = time.strftime("%x")
-        autogit(filepath, localtime)
-    elif option == "-m":
-        filepath = sys.argv[2]	
+        filename = sys.argv[2]
+        autogit(filepath, localdate, filename, multibool)
+    elif arglength == 3 and option == "-m":
+        filepath = sys.argv[2]
         message = sys.argv[3]
-        print message
-        autogit(filepath, message)
+        filename = sys.argv[4]
+        autogit(filepath, message, filename, multibool)
+    elif arglength > 3 and option == "-s":
+        filelist = sys.argv[3:]
+        filepath = sys.argv[2]
+        multibool = 1;
+        autogit(filepath, localdate, filelist, multibool)
     elif option == "-h":
         print help()
     else:
@@ -47,6 +62,6 @@ try:
 
     trashcan.close()
     os.chdir(actualpath)
-except (SyntaxError, KeyboardInterrupt):
-    print "Something happened (use autogit -h for help)."
+except (SyntaxError, KeyboardInterrupt, IndexError):
+    print "Something happened (use ./autogit -h for help)."
     print "If the that didn't please report the bug."
